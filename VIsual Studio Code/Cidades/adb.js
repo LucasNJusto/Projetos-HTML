4c486c2f43c447edf87697a3a496134dea73e7b0
LUCAS NOVAES JUSTO <lucas.njusto@senacsp.edu.br>
2023-11-13 14:19:10 -0300

    Primeiro Commit

---
+++ VIsual Studio Code/Cidades/adb.js
@@ -0,0 +1,2 @@
+// Exibir um alerta ao entrar na página
+alert("Bem-vindo à Historia das cidades do ABD!");
---
+++ VIsual Studio Code/Receita/receita.js
@@ -0,0 +1,2 @@
+// Exibir um alerta ao entrar na página
+alert("Bem-vindo à receita de Lasanha a Bolonhesa! (esse é o pop-up)");
